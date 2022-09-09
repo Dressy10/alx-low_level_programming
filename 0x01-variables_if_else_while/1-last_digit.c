de35f38d852397de238277349e56af76bafaeb4d
Samuel <samueldressman20@gmail.com>
2022-09-09 10:06:10 -0700

    My C variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-#include<stdio.h>
+#include <stdio.h>
 /**
  *  main - To determine if the digit is either greater than (5), less than (6), or equals to (0).
  *

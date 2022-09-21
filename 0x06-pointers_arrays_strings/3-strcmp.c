a62945a399229f4f73eaa41d1cd3672bd50743b9
Samuel <samueldressman20@gmail.com>
2022-09-21 12:38:36 -0700

    Fixing This

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,13 +8,13 @@
  **/
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, op = 0;
+	int i = 0, d = 0;
 
-	while (op == 0)
+	while (d == 0)
 	{
 		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
 			break;
-		op = *(s2 + 1) - *(s2 + 1);
+		d = *(s1 + i) - *(s2 + i);
 		i++;
 	}
 	return (0);

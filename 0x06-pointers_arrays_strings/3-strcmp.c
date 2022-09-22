f95b5abcbbcb579e2c18e915d69ba781e6522cfa
Samuel <samueldressman20@gmail.com>
2022-09-22 11:16:13 -0700

    Fixing this. It works

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,14 +8,29 @@
  **/
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, d = 0;
+	int i = 0, diff = 0;
 
-	while (d == 0)
+	while (1)
 	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+		if (s1[i] == '\0' && s2[i] == '\0')
 			break;
-		d = *(s1 + i) - *(s2 + i);
-		i++;
+		else if (s1[i] == '\0')
+		{
+			diff = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
 	}
-	return (0);
+	return (diff);
 }

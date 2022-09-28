ce00fb2fc7db1b01e583eb82eb276768515d8e0d
Samuel <samueldressman20@gmail.com>
2022-09-28 04:05:58 -0700

    My C recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * factorial - returns the factorial o an integer.
+ * factorial - returns the factorial of an integer.
  * @n: number of integer.
  * Return: factorial of an integer.
  **/
 int factorial(int n)
 {
 	if (n < 0)
-		return (0);
+		return (-1);
 	else if (n == 0)
 		return (1);
 	else

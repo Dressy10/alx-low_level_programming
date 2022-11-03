39b049c359c98c3463e9ca70153cd501c820dff6
Samuel <samueldressman20@gmail.com>
2022-11-03 09:56:50 -0700

    Fixing updates

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,7 +6,8 @@
  *
  * Return: 1 if the file exists. -1 if the fails does not exist
  * or if it fails.
- */int append_text_to_file(const char *filename, char *text_content)
+ */
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	int nletters;
